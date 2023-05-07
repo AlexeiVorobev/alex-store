47fe357a92918cf6946b3a53677b289c9b53ec40
Alex <alexthetaffer@gmail.com>
2023-05-07 11:31:02 +0300

    Style categories section

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import { categories } from "../data"
+import CategoryItem from "./CategoryItem"
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`;
+
+export default function Categories() {
+  return (
+    <Container>
+        {categories.map(item => (
+            <CategoryItem key={item.id} item={item} />
+        ))}
+    </Container>
+  )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 }
